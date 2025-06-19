9be3fecb697f75d627988d66e0161ac8d79ec75e
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-06-19 19:41:49 -0300

    feat(frontend): creates cta blue button variant as component

---
+++ frontend/src/components/CtaBlueButton/CtaBlueButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CtaBlueButton.module.css";
+
+const CtaBlueButton: React.FC = () => {
+  return <button className={styles.CtaBlueButton}>{}</button>;
+};
+
+export default CtaBlueButton;
