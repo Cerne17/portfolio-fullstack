0a5ea37839456842f6b22b24ea4dbffc7aa91d76
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-06-19 15:27:32 -0300

    feat(frontend): creates a simple hero section

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -5,6 +5,21 @@ const Hero: React.FC = () => {
   return (
     <section className={styles.heroSection}>
       <h3 className={styles.subsectionTitle}>Who's Miguel Cerne?</h3>
+      <div className={styles.container}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          minus nihil nesciunt animi veritatis officia commodi illo! Numquam
+          voluptate consequuntur expedita, quisquam esse recusandae molestiae
+          facilis corporis aut sit repellat exercitationem magni ipsam
+          aspernatur nemo animi culpa ducimus fugit eligendi quae aperiam in.
+          Exercitationem corrupti minus, nesciunt error nostrum laudantium!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
+          explicabo optio aspernatur ad quisquam maiores! Excepturi a hic dicta!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, eos.
+        </p>
+        {/* Banner Placeholder */}
+        <div className={styles.bannerPlaceholder}></div>
+      </div>
     </section>
   );
 };
