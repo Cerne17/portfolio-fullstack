a0b9bac919b67c3e478865d60f5d5a4892a5d608
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-06-19 19:42:05 -0300

    feat(frontend): creates cta transparent button variant as component

---
+++ frontend/src/components/CtaTransparentButton/CtaTransparentButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CtaTransparentButton.module.css";
+
+const CtaTransparentButton: React.FC = () => {
+  return <button className={styles.CtaBlueButton}>{}</button>;
+};
+
+export default CtaTransparentButton;
