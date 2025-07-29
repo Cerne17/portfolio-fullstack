9f2f0a97f07717ad37d2e689b4c7f3af7484f8f6
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-07-29 13:27:47 -0700

    feat: includes contact form to home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import AboutMe from "./components/AboutMe/AboutMe";
+import ContactForm from "./components/ContactMe/ContactMe";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <AboutMe />
+      <ContactForm />
     </div>
   );
 }
