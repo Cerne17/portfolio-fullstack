8cb7752673a0cb2937c3245ed9f0c6083c07b27a
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-11-20 11:29:10 -0300

    test: creates initial Footer Section tests

---
+++ frontend/src/sections/Footer/Footer.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
+
+describe('Footer Component', () => {
+  test('renders copyright text', () => {
+    render(<Footer />);
+    const currentYear = new Date().getFullYear();
+    const copyrightElement = screen.getByText(new RegExp(`© ${currentYear} Miguel Cerne`, 'i'));
+    expect(copyrightElement).toBeInTheDocument();
+  });
+
+  test('renders social links', () => {
+    render(<Footer />);
+    const githubLink = screen.getByLabelText(/GitHub/i);
+    const linkedinLink = screen.getByLabelText(/LinkedIn/i);
+    const emailLink = screen.getByLabelText(/Email/i);
+
+    expect(githubLink).toBeInTheDocument();
+    expect(linkedinLink).toBeInTheDocument();
+    expect(emailLink).toBeInTheDocument();
+    
+    expect(githubLink).toHaveAttribute('href', 'https://github.com/Cerne17');
+    expect(linkedinLink).toHaveAttribute('href', 'https://www.linkedin.com/in/miguelcerne/');
+    expect(emailLink).toHaveAttribute('href', 'mailto:miguelcerne.dev@gmail.com');
+  });
+});
