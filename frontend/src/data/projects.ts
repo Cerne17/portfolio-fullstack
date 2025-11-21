7a8089ad6c491f4f9db7b73f37c10dcfd65c0194
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-11-21 11:46:14 -0300

    feat: adds the projects ts file

--- frontend/src/data/projects.ts
+++ frontend/src/data/projects.ts
@@ -4,17 +4,18 @@ import {
   SiExpress,
   SiNodedotjs,
   SiFigma,
+  SiPython
 } from "react-icons/si";
 
 const projects: IProject[] = [
   {
     id: 1,
-    title: "Project A",
-    description: "This is a project made to learn the latest languages by building an app.",
-    techStack: ["React", "Express", "Node.js"],
-    demoUrl: "https://www.example.com",
-    repoUrl: "https://www.github.com",
-    image: "history.png",
+    title: "Database Population",
+    description: "Designed to solve the challenge of populating complex relational schemas, this project is a fully automated data pipeline built with Python. It generates realistic synthetic data by orchestrating inputs from the 'Faker' library and consuming official Brazilian government APIs. The system creates coherent, interconnected entities (such as user profiles, valid addresses, and corporate structures) streamlining the process of seeding databases for testing and development.",
+    techStack: ["Python"],
+    // demoUrl: "https://www.example.com",
+    repoUrl: "https://github.com/Cerne17/Database-Population/tree/main",
+    image: "DatabasePopulationBanner.png",
   },
   {
     id: 2,
@@ -40,6 +41,7 @@ export const projectSkills = {
   "React": { icon: SiReact, stack: "frontend" },
   "Express": { icon: SiExpress, stack: "backend" },
   "Node.js": { icon: SiNodedotjs, stack: "backend" },
+  "Python": { icon: SiPython, stack: "backend" },
   "Figma": { icon: SiFigma, stack: "frontend" }, // Assuming Figma is frontend/design
 };
 
